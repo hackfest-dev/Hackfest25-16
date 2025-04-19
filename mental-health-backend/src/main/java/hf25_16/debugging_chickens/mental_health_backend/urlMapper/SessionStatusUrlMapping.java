a5333847760400fb034df2c3abdbc3802a0bf2ef
RAJAY04 <nnm22ad041@nmamit.in>
2025-04-19 19:28:11 +0530

    feat: sessionCount related apis, session list apis

--- mental-health-backend/src/main/java/hf25_16/debugging_chickens/mental_health_backend/urlMapper/SessionStatusUrlMapping.java
+++ mental-health-backend/src/main/java/hf25_16/debugging_chickens/mental_health_backend/urlMapper/SessionStatusUrlMapping.java
@@ -6,4 +6,5 @@ import lombok.NoArgsConstructor;
 public class SessionStatusUrlMapping {
     public static final String BASE_URL = "/api/v1/session-status";
     public static final String FILTER_SESSIONS = BASE_URL + "/filter";
+    public static final String USER_SESSION_TAG_COUNTS = BASE_URL + "/count";
 }
